ace7171e82d9ed23588c38604c54b40066096cb2
akandikatla <aaronkandikatla@gmail.com>
2022-04-18 09:49:45 -0500

    added name

--- shepherd/src/views/HomeView.vue
+++ shepherd/src/views/HomeView.vue
@@ -1,15 +1,21 @@
 <template>
   <div>
     <sign-in-out></sign-in-out>
-    <h1 class="title is-5"><strong>Let's begin</strong></h1>
+    <h1 class="title is-5"><strong>Let's begin, {{display}}</strong></h1>
   </div>
 </template>
 
 <script>
 import SignInOut from "@/components/SignInOut";
+import {auth} from "@/firebaseConfig";
 export default {
   name: "HomeView",
-  components: {SignInOut}
+  components: {SignInOut},
+  data() {
+    return {
+      display: auth.currentUser.displayName
+    };
+  }
 };
 </script>
 
