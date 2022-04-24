42c460b7be7e41fbef77930ffff57f19d703c281
akandikatla <aaronkandikatla@gmail.com>
2022-04-24 17:39:40 -0500

    removed some toolbar buttons

--- shepherd/src/components/NoteBody.vue
+++ shepherd/src/components/NoteBody.vue
@@ -29,7 +29,7 @@ export default {
         toolbarItems: [
           ["hr", "quote"],
           ["task"],
-          ["image", "link"],
+          ["link"],
           ["code", "codeblock"]
         ]
       }
