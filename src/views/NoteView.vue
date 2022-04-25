d75231ee80e88250f4999b9ca1067f6706284dc2
Nikunj Chawla <24688726+nik312123@users.noreply.github.com>
2022-04-25 17:42:56 -0500

    Removed unused displayTags code in NoteView

--- src/views/NoteView.vue
+++ src/views/NoteView.vue
@@ -54,18 +54,6 @@ export default {
         };
     },
     methods: {
-        displayTags: function(tags) {
-            let totalChars = 20;
-            let i = 0;
-            let displayedTags = [];
-            while(totalChars > 0 && i < tags.length) {
-                totalChars -= tags[i].length;
-                displayedTags.push(tags[i]);
-                i += 1;
-            }
-            let remainingTags = tags.length - displayedTags.length;
-            return [displayedTags, remainingTags];
-        },
         timeSince: function(date) {
             if(typeof date !== 'object') {
                 date = new Date(date);
