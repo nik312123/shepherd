4eddd7713e4d49783759e513e86fb6f12fea85d3
Nikunj Chawla <24688726+nik312123@users.noreply.github.com>
2022-04-25 16:10:08 -0500

    Explained Inter font import from Google

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,8 @@
 </template>
 
 <style>
-/*noinspection CssUnknownTarget*/
+/* Imports the Inter font from Google */
+/* noinspection CssUnknownTarget */
 @import url("https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap");
 
 html {
