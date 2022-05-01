397c79539055fa1353d0b25fbcafa3825fd113d9
Nikunj Chawla <24688726+nik312123@users.noreply.github.com>
2022-04-30 19:26:35 -0500

    Added PWA app name and theme color

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
 const {defineConfig} = require('@vue/cli-service');
+
 module.exports = defineConfig({
-    transpileDependencies: true
+    transpileDependencies: true,
+    pwa: {
+        name: 'Shepherd',
+        themeColor: '#1A2434',
+    }
 });
