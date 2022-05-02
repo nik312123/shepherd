0bdda99c29a31a368eac0c736c5018cbd66d2655
Nikunj Chawla <24688726+nik312123@users.noreply.github.com>
2022-05-01 19:06:05 -0500

    Reformatted server files

--- server/messageServer.js
+++ server/messageServer.js
@@ -1,10 +1,9 @@
 let admin = require('firebase-admin');
-let serviceAccount = require("./shepherd-be6df-firebase-adminsdk-g53dz-efd58d9b21.json");
-
+let serviceAccount = require('./shepherd-be6df-firebase-adminsdk-g53dz-efd58d9b21.json');
 
 let app = admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-})
+    credential: admin.credential.cert(serviceAccount)
+});
 let messaging = app.messaging();
 let db = app.firestore();
 
@@ -13,28 +12,29 @@ const sendNotifications = (messages, docIds) => {
         .then((response) => {
             console.log('Message status', JSON.stringify(response));
             response.responses.forEach((response, index) => {
-                if (response.success) {
+                if(response.success) {
                     db.collection('notes').doc(docIds[index]).update({
                         'notified': true
                     }).then(() => {
-                        console.log("Notified for docId: ", docIds[index]);
+                        console.log('Notified for docId: ', docIds[index]);
                     }).catch((err) => {
-                        console.log("Error updating docId: ", docIds[index], err);
+                        console.log('Error updating docId: ', docIds[index], err);
                     });
-                } else {
-                    console.log("Error sending message: ", docIds[index], JSON.stringify(response.error));
+                }
+                else {
+                    console.log('Error sending message: ', docIds[index], JSON.stringify(response.error));
                 }
             });
         })
         .catch((error) => {
             console.log('Error sending message:', error);
         });
-}
+};
 
-db.collection("notes")
-    .where("notified", "==", false)
-    .where("reminderDateTime", "<=", new Date(Date.now()))
-    .where("isTrash", "==", false)
+db.collection('notes')
+    .where('notified', '==', false)
+    .where('reminderDateTime', '<=', new Date(Date.now()))
+    .where('isTrash', '==', false)
     .get()
     .then((querySnapshot) => {
         let messages = [];
@@ -44,15 +44,15 @@ db.collection("notes")
             let payload = {
                 notification: {
                     title: data.title,
-                    body: data.body.length > 50 ? data.body.substring(0, 50)+"..." : data.body,
+                    body: data.body.length > 50 ? data.body.substring(0, 50) + '...' : data.body
                 },
                 webpush: {
                     fcmOptions: {
-                        link: "https://shepherd-be6df.firebaseapp.com/notes/" + data.id
+                        link: 'https://shepherd-be6df.firebaseapp.com/notes/' + data.id
                     }
                 },
                 data: {
-                  noteId: doc.id
+                    noteId: doc.id
                 },
                 token: data.messageToken
             };
@@ -61,10 +61,11 @@ db.collection("notes")
         });
         if(messages.length > 0) {
             sendNotifications(messages, docIds);
-        } else {
-            console.log("No messages to send");
+        }
+        else {
+            console.log('No messages to send');
         }
     })
     .catch((error) => {
-        console.log("Error getting documents: ", error);
+        console.log('Error getting documents: ', error);
     });
