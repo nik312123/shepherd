6df7965d7d2509daa8773cc2eab7882e3914cfef
Nikunj Chawla <24688726+nik312123@users.noreply.github.com>
2022-05-02 06:13:52 -0500

    Removed unnecessary service work console.log messages for PWA

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -4,28 +4,14 @@ import {register} from 'register-service-worker';
 
 if(process.env.NODE_ENV === 'production') {
     register(`${process.env.BASE_URL}service-worker.js`, {
-        ready: function() {
-            console.log(
-                'App is being served from cache by a service worker.\n' +
-                'For more details, visit https://goo.gl/AFskqB'
-            );
-        },
-        registered: function() {
-            console.log('Service worker has been registered.');
-        },
-        cached: function() {
-            console.log('Content has been cached for offline use.');
-        },
-        updatefound: function() {
-            console.log('New content is downloading.');
-        },
+        ready: function() {},
+        registered: function() {},
+        cached: function() {},
+        updatefound: function() {},
         updated: function() {
-            console.log('New content is available; please refresh.');
             window.location.reload();
         },
-        offline: function() {
-            console.log('No internet connection found. App is running in offline mode.');
-        },
+        offline: function() {},
         error: function(error) {
             console.error('Error during service worker registration:', error);
         }
